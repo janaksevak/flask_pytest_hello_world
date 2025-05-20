9985610a2c103da714e8dc6fb04badc89b675d56
rastafarian <janakmsevak@gmail.com>
2025-05-20 05:08:29 -0700

    Mock Deploy Flow Added

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,33 +1,3 @@
-import time
-import pytest
-from selenium.webdriver.common.by import By
-
-def test_toggle_world_text(selenium):
-    # Make sure Flask app is running at this URL
-    selenium.get("http://127.0.0.1:5000")
-
-    # Wait for the page to load
-    selenium.implicitly_wait(10)
-
-    # Find elements by their IDs
-    text = selenium.find_element(By.ID, "myText")
-    button = selenium.find_element(By.ID, "myButton")
-
-    # Initial state check
-    assert text.text == "Hello Current World"
-    assert button.text == "Go to New World"
-
-    # Click the button
-    button.click()
-    time.sleep(0.5)  # Wait for DOM update
-
-    # Verify new state
-    assert text.text == "Hello New World"
-    assert button.text == "Go to Old World"
-
-
-
-"""
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -41,7 +11,7 @@ def browser():
 
     # Set up Chrome options to run in headless mode
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    # chrome_options.add_argument("--headless")  # Run Chrome in headless mode
     chrome_options.add_argument("--no-sandbox") 
     chrome_options.add_argument("--disable-dev-shm-usage")   # Prevent shared memory crashes
     chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
@@ -56,7 +26,6 @@ def browser():
 
 def test_toggle_world_text(browser):
     # Make sure Flask app is running at this URL
-    # browser.get("http://localhost:5000") 
     browser.get("http://127.0.0.1:5000") 
 
     # Add a wait to ensure the page loads
@@ -77,4 +46,3 @@ def test_toggle_world_text(browser):
     # Check updated content
     assert text.text == "Hello New World"
     assert button.text == "Go to Old World"
-"""
